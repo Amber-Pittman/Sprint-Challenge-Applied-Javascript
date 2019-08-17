156713d8683992889aa66f4321b96632d8b2ae75
Amber-Pittman <amber@amberpittman.com>
2019-08-16 20:34:08 -0400

    Created Tabs Get Request

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -1,9 +1,34 @@
-// Step 2: Create Tabs
-// -----------------------
-// Using axios send a GET request to the address: https://lambda-times-backend.herokuapp.com/topics
-// Once the data is returned console.log it and review the structure.
-// Iterate over the topics creating a new Tab component and add it to the DOM
-// under the .topics element.
-//
-//  The tab component should look like this:
-//    <div class="tab">topic here</div>
+/* Step 2: Create Tabs
+-----------------------
+Using axios send a GET request to the address:
+ https://lambda-times-backend.herokuapp.com/topics
+Once the data is returned console.log it and review the structure.
+Iterate over the topics creating a new Tab component and add it to 
+the DOM under the .topics element.
+
+ The tab component should look like this:
+   <div class="tab">topic here</div>
+*/
+
+const tabsContr = document.querySelector(".topics");
+
+axios.get("https://lambda-times-backend.herokuapp.com/topics")
+  .then((response) => {
+      let newArray = response.data.topics;
+      console.log(newArray);
+      newArray.forEach(element => {
+          tabsContr.appendChild(Tabs(element));
+      });
+  })
+  .catch(error => {
+      console.error(error);
+      console.log("Network request unsuccessful");
+  });
+
+  function Tabs(response) {
+      const tabs = document.createElement("div");
+      tabs.classList.add("tab");
+      tabs.textContent = response;
+
+      return tabs;
+  }
